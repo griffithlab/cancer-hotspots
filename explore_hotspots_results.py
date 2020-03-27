61530a3f2ba508de70cd0bf8fd4959ccd34580f1
Megan Richters <meganrichters4@gmail.com>
2020-03-27 10:28:45 -0500

    added score_intervals function

--- explore_hotspots_results.py
+++ explore_hotspots_results.py
@@ -1,5 +1,5 @@
 """
-Usage: python3 explore_hotspots_results.py input_dir output_file
+Usage: python3 explore_hotspots_results.py <input_dir> <output_file>
 
 Parses hotspot pVACtools results files for further analysis
 """
@@ -16,16 +16,9 @@ def identify_neoantigens(df):
         df (dataframe): pVACtools total predictions df
 
     Returns:
-        tuple: df for classI neoantigens, df for classII neoantigens, dictionary with score-based output
+        tuple: df for classI neoantigens, df for classII neoantigens
     """
-    # score-based output
-    df_low_score = df_variable.loc[df_variable["Median MT Score"] <= 50]
-    df_med_score = df_variable.loc[(df_variable["Median MT Score"] > 50) & (df_variable["Median MT Score"] <= 500)]
-    df_high_score = df_variable.loc[(df_variable["Median MT Score"] > 500) & (df_variable["Median MT Score"] <= 1000)]
-    score = [("low", len(df_low_score)), ("med", len(df_med_score)), ("high", len(df_high_score))]
-    score_dict = {x:y for (x,y) in score}
-    # anchor neoantigen output
-    df_neoantigen = pd.concat([df_low_score, df_med_score])
+    df_neoantigen = df.loc[df["Median MT Score"] <= 500]
     class_I = [i for i in df_neoantigen["HLA Allele"] if any(x in i for x in ["HLA-A", "HLA-B", "HLA-C"])]
     class_II = [i for i in df_neoantigen["HLA Allele"] if any(x in i for x in ["DP", "DQ", "DR"])]
     index_list_classI = []
@@ -58,46 +51,65 @@ def identify_neoantigens(df):
             index_list_classII.append(row_index)
     df_neo_output_classI = df_neoantigen.loc[index_list_classI, :]
     df_neo_output_classII = df_neoantigen.loc[index_list_classII, :]
-    return df_neo_output_classI, df_neo_output_classII, score_dict
+    return df_neo_output_classI, df_neo_output_classII
+
+def score_intervals(df, message):
+    """Find number of predictions per score intervals for pVACtools predictions df
+
+    Args:
+        df (dataframe): pVACtools predictions df
+        message (str): descriptive prefix for resulting column name (ex: "ClassI Neo" for Class I Neoantigens)
+
+    Returns:
+        dataframe: columns = score intervals <50, 50-500, 500-1000, row = number of predictions per score interval  
+    """
+    df_low_score = df.loc[df["Median MT Score"] <= 50]
+    df_med_score = df.loc[(df["Median MT Score"] > 50) & (df["Median MT Score"] <= 500)]
+    df_high_score = df.loc[(df["Median MT Score"] > 500) & (df["Median MT Score"] <= 1000)]
+    score = [(f'{message} Score < 50nM', len(df_low_score)), (f'{message} Score 50-500nM', len(df_med_score)), (f'{message} Score 500-1000nM', len(df_high_score))]
+    neo_scores = pd.DataFrame({x:y for (x,y) in score}, index=[0])
+    return neo_scores
 
 def parse_predictions(df, messages):
     """Return general stats of df predictions
 
     Args:
         df (dataframe): pVACtools df
-        messages (list): descriptive name + abbreviation for resulting column ex: [Neoantigens, Neo]
+        messages (list): descriptive name + abbreviation for resulting column ex: ["Neoantigens", "Neo"]
 
     Returns:
         dataframe: column = name, row = count
     """
-    # explore df.variable
     variable_dict = {}
     columns = ["HLA Allele", "MT Epitope Seq"]
     variable_dict[messages[0]] = len(df)
     for item in columns:
         item_set = df[item].unique()
-        item_name = f'item {messages[1]}'
+        item_name = f'{messages[1]} {item}'
         variable_dict[item_name] = [len(item_set)]
         output_df = pd.DataFrame(variable_dict, index=[0])
     return output_df
 
-def find_min_prediction(df):
+def find_min_prediction(df, hla_class):
     """Find the best scoring prediction for class I and II HLA alleles
 
     Args:
         df (dataframe): pVACtools class I/II neoantigens df
+        hla_class (string): "ClassI" or "ClassII" 
 
     Returns:
         dataframe: columns = name, row = minimum prediction attributes
     """
-    # minimun median mt score - full line
     min_score_line = df[df["Median MT Score"] == df["Median MT Score"].min()]
     columns = ["HLA Allele", "MT Epitope Seq", "Median MT Score"]
     min_dict = {}
+    new_columns = []
     for item in columns:
+        new_columns.append(f'{hla_class} {item}')
         min_dict[item] = min_score_line[item].values[0]
     output_df = pd.DataFrame(min_dict, index=[0])
-    return output_df if len(df) != 0 else pd.DataFrame({columns[0]: "NA", columns[1]: "NA", columns[2]: "NA"}, index=[0])
+    output_df.rename(columns = {x:y for x,y in zip(columns, new_columns)}, inplace = True)
+    return output_df if len(df) != 0 else pd.DataFrame({new_columns[0]: "NA", new_columns[1]: "NA", new_columns[2]: "NA"}, index=[0])
 
 def length_count(df, message):
     """Find the number of mut sequences that correspond to each peptide length
@@ -107,7 +119,7 @@ def length_count(df, message):
         message (str): descriptive prefix for resulting column name (ex: Neo for neoantigen)
     
     Return:
-        df (dataframe): columns = message + peptide length, row = mut sequence count per peptide length
+        dataframe: columns = message + peptide length, row = mut sequence count per peptide length
     """
     unique_seqs = df['MT Epitope Seq'].unique().tolist()
     len_seqs = [len(x) for x in unique_seqs]
@@ -121,35 +133,43 @@ working_dir=os.getcwd()
 output_file=f'{working_dir}/{output_filename}'
 
 open_files = [f for f in sorted(os.listdir(input_dir)) if f.endswith("_TUMOR.all_epitopes.tsv")]
-#open_files = [f for f in sorted(os.listdir(f'{working_dir}/{input_dir}/')) if f.endswith("_TUMOR.all_epitopes.tsv")] 
 for file1 in open_files:
     df_input = pd.read_csv(f'{input_dir}/{file1}', sep='\t')
-    # constant per file
+    # constant info per file
     df_constant = df_input[["Chromosome", "Start", "Stop", "Reference", "Variant", "Transcript", "Ensembl Gene ID", "Variant Type", "Mutation", "Protein Position", "Gene Name", "HGVSp"]]
     constant_line = df_constant.drop_duplicates()
-    # unique for each line
+    # unique info per line
     df_variable = df_input[["HLA Allele", "Peptide Length", "Sub-peptide Position", "Mutation Position", "MT Epitope Seq", "WT Epitope Seq", "Median MT Score", "Median WT Score", "Median Fold Change"]]
+    df_variable.loc[df_variable["Median MT Score"] <= 50]
     df_variable = df_variable.drop_duplicates()
+    df_classI = df_variable.loc[df_variable["HLA Allele"].str.contains("HLA-A|HLA-B|HLA-C")]
+    df_classII = df_variable.loc[~df_variable["HLA Allele"].str.contains("HLA-A|HLA-B|HLA-C")]
 
     file_info = pd.DataFrame({"File Name": file1},index=[0])
     constant_line = df_constant.drop_duplicates()
     
-    neo_classI, neo_classII, neo_score_dict = identify_neoantigens(df_variable)
-    
-    total_pred = parse_predictions(df_variable, ["Predictions", "Total"])
-    classI_pred = parse_predictions(neo_classI, ["Neo Class I", "ClassI"])
-    classII_pred = parse_predictions(neo_classII, ["Neo Class II", "ClassII"])
+    neo_classI, neo_classII = identify_neoantigens(df_variable)
 
-    classI_min = find_min_prediction(neo_classI)
-    classII_min = find_min_prediction(neo_classII)
+    total_scores = score_intervals(df_variable, "Total")
+    classI_scores = score_intervals(df_classI, "ClassI")
+    classII_scores = score_intervals(df_classII, "Class II")
+
+    total_pred = parse_predictions(df_variable, ["Total Predictions", "Total"])
+    classI_pred = parse_predictions(df_classI, ["Class I Predictions", "ClassI"])
+    classI_neo_pred = parse_predictions(neo_classI, ["Neo Class I Predictions", "Neo ClassI"])
+    classII_pred = parse_predictions(df_classII, ["Class II Predictions", "ClassII"])
+    classII_neo_pred = parse_predictions(neo_classII, ["Neo Class II Predictions", "Neo ClassII"])
+
+    classI_min = find_min_prediction(neo_classI, "ClassI")
+    classII_min = find_min_prediction(neo_classII, "ClassII")
     
     total_len = length_count(df_variable, "Seq")
     neo_len = length_count(pd.concat([neo_classI, neo_classII]), "Neo")
 
-    final_df_list = [file_info, constant_line, total_pred, classI_pred, classII_pred, classI_min, classII_min, total_len, neo_len] 
-    final_write_df = pd.DataFrame(final_df_list, index=[0])
+    final_df_list = [file_info, constant_line, total_pred, classI_pred, classI_neo_pred, classII_pred, classII_neo_pred, total_scores, classI_scores, classII_scores, classI_min, classII_min, total_len, neo_len]
+    df_merged = pd.concat(final_df_list, join='outer', axis=1)
 
     if os.path.isfile(output_file):
-        final_write_df.to_csv(output_file, mode = 'a', index=False, header=None)
+        df_merged.to_csv(output_file, mode = 'a', index=False, header=None)
     else:
-        final_write_df.to_csv(output_file, mode = 'w', index=False)
+        df_merged.to_csv(output_file, mode = 'w', index=False)
