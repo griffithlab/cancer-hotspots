ec78ea39140faf39ba993eb8e8532c5a30236e42
abasu649 <54504310+abasu649@users.noreply.github.com>
2020-10-08 13:38:41 -0700

    Update split_class_hotspot_graph_data_filtered.py

--- Anamika New Workflow/split_class_hotspot_graph_data_filtered.py
+++ Anamika New Workflow/split_class_hotspot_graph_data_filtered.py
@@ -13,6 +13,7 @@ directory = '/gscmnt/gc2142/griffithlab/abasu/gene_filtered_neo/'
 path = directory + gene + ".tsv"
 # dataframe of alleles, binding affinities, seq_length, mutation position, wt score, fold_change for all neoantigens for 1 gene
 df = pd.read_csv(path, delimiter='\t', sep='\t',header=None)
+df = df.drop_duplicates()
 df.columns = ["Allele", "Seq_len", "Mut_pos", "MT_score", "WT_score", "Fold_change"]
 
 row_remove = []
@@ -44,42 +45,38 @@ df = df.drop(df.index[row_remove])
 # as of now, df2 contains all neoantigens with BA < 1000nm that have passed anchor mutation filters 
 
 # split class 1 and class 2 into different dataframes
-class1_allele = []
-class2_allele = []
-class1_score = []
-class2_score = []
+class1_0_50 = 0
+class1_50_500 = 0
+
+class2_0_50 = 0
+class2_50_500 = 0
+
 for r in range(0, len(df)):
     if "D" in df.iloc[r, 0]:
-        class2_allele.append(df.iloc[r, 0])
-        class2_score.append(df.iloc[r, 3])
+        if df.iloc[r,3] <= 50:
+            class2_0_50 += 1
+        elif df.iloc[r,3] <= 500:
+            class2_50_50 += 1
     else:
-        class1_allele.append(df.iloc[r, 0])
-        class1_score.append(df.iloc[r, 3])
+        if df.iloc[r,3] <= 50:
+            class1_0_50 += 1
+        elif df.iloc[r,3] <= 500:
+            class1_50_50 += 1
+        
 
-class1 = pd.DataFrame(class1_allele, columns =['Allele'])
-class1['Score'] = class1_score
-class2 = pd.DataFrame(class2_allele, columns =['Allele'])
-class2['Score'] = class2_score
 
-bins = [0, 50, 500]
-
-s1 = pd.cut(class1['Score'], bins=bins).value_counts()
-s2 = pd.cut(class2['Score'], bins=bins).value_counts()
-s1.index = s1.index.astype(str)
-s2.index = s2.index.astype(str)
-
-graph_d_c1 = '/gscmnt/gc2142/griffithlab/abasu/split_class_hotspot_graph_data_filtered/' + gene + '_Graph_D_Class1_Data.tsv'
-graph_d_c2 = '/gscmnt/gc2142/griffithlab/abasu/split_class_hotspot_graph_data_filtered/' + gene + '_Graph_D_Class2_Data.tsv'
+graph_d_c1 = '/gscmnt/gc2142/griffithlab/abasu/10_2020/hotspot_ba_counts_class_split/' + gene + '_Class1_Data.tsv'
+graph_d_c2 = '/gscmnt/gc2142/griffithlab/abasu/10_2020/hotspot_ba_counts_class_split/' + gene + '_Class2_Data.tsv'
 
 with open(graph_d_c1, 'wt') as out_file:
     tsv_writer = csv.writer(out_file, delimiter='\t')
-    tsv_writer.writerow([gene, s1.loc[['(0, 50]']][0],"0-50 nM"])
-    tsv_writer.writerow([gene, s1.loc[['(50, 500]']][0],"50-500 nM"])
+    tsv_writer.writerow([gene, class1_0_50 ,"0-50 nM"])
+    tsv_writer.writerow([gene, class1_50_500,"50-500 nM"])
 
 with open(graph_d_c2, 'wt') as out_file:
     tsv_writer = csv.writer(out_file, delimiter='\t')
-    tsv_writer.writerow([gene, s2.loc[['(0, 50]']][0],"0-50 nM"])
-    tsv_writer.writerow([gene, s2.loc[['(50, 500]']][0],"50-500 nM"])
+    tsv_writer.writerow([gene, class2_0_50,"0-50 nM"])
+    tsv_writer.writerow([gene, class2_50_500,"50-500 nM"])
 
 # dataframe of allele frequencies scraped from allelefrequencies.net
 freqs = pd.read_csv('/gscmnt/gc2142/griffithlab/abasu/Global_HLA_Freq.csv')
@@ -114,8 +111,8 @@ s2 = pd.cut(class2['Frequency'], bins=bins_2).value_counts()
 s1.index = s1.index.astype(str)
 s2.index = s2.index.astype(str)
 
-graph_e_c1 = '/gscmnt/gc2142/griffithlab/abasu/split_class_hotspot_graph_data_filtered/' + gene + '_Graph_E_Class1_Data.tsv'
-graph_e_c2 = '/gscmnt/gc2142/griffithlab/abasu/split_class_hotspot_graph_data_filtered/' + gene + '_Graph_E_Class2_Data.tsv'
+graph_e_c1 = '/gscmnt/gc2142/griffithlab/abasu/10_2020/hotspot_freq_counts_class_split/' + gene + '_Class1_Data.tsv'
+graph_e_c2 = '/gscmnt/gc2142/griffithlab/abasu/10_2020/hotspot_freq_counts_class_split/' + gene + '_Class2_Data.tsv'
 
 with open(graph_e_c1, 'wt') as out_file:
     tsv_writer = csv.writer(out_file, delimiter='\t')
